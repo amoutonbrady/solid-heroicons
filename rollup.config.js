e2fcd7c69fef34370ad84b4eda5807fa465b904d
Alexandre <amoutonbrady@gmail.com>
2021-05-15 12:39:27 +0200

    :art: Use rollup-preset-solid + update dependencies + full ESM

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,3 @@
-import del from "rollup-plugin-delete";
-import { babel } from "@rollup/plugin-babel";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import withRollup from 'rollup-preset-solid'
 
-const extensions = [".ts", ".tsx", ".js"];
-
-/**
- * @type {import('rollup').RollupOptions}
- */
-const config = {
-  input: "src/icon.tsx",
-  output: [
-    { dir: "dist/cjs", format: "cjs" },
-    { dir: "dist/esm", format: "esm" },
-  ],
-  plugins: [
-    del({ targets: ["dist/*"] }),
-    nodeResolve({ extensions, browser: true }),
-    babel({
-      babelHelpers: "bundled",
-      extensions,
-      presets: ["@babel/preset-typescript", "babel-preset-solid"],
-    }),
-  ],
-  external: ["solid-js", "solid-js/web"],
-};
-
-export default config;
+export default withRollup ({ input: "src/icon.tsx" });
--- scripts/fetchIcons.ts
+++ scripts/fetchIcons.ts
@@ -1,99 +1,91 @@
-import { join, parse } from "path";
-import { readdir, readFile, outputFile, remove, removeSync } from "fs-extra";
-import { camelCase } from "change-case";
 import dd from "dedent";
 import degit from "degit";
+import { join, parse } from "path";
+import { camelCase } from "change-case";
+import { readdir, readFile, outputFile, remove, removeSync } from "fs-extra";
 
 const TMP = join(process.cwd(), "tmp");
+
 const SOLID_SRC = "tailwindlabs/heroicons/optimized/solid";
 const SOLID_DIST = join(TMP, "solid");
+
 const OUTLINE_SRC = "tailwindlabs/heroicons/optimized/outline";
 const OUTLINE_DIST = join(TMP, "outline");
 
-async function main() {
-  removeSync(SOLID_DIST);
-  removeSync(OUTLINE_DIST);
-  const [solidGit, outlineGit] = [SOLID_SRC, OUTLINE_SRC].map((repo) =>
-    degit(repo, { cache: false, verbose: true, force: true })
-  );
-  await solidGit.clone(SOLID_DIST);
-  await outlineGit.clone(OUTLINE_DIST);
+// Start the whole machinery
+main().catch((e) => {
+	console.error(e);
+	process.exit(1);
+});
 
-  await generateIcons({
-    path: SOLID_DIST,
-    name: "solid",
-    outline: false,
-  });
-  await generateIcons({
-    path: OUTLINE_DIST,
-    name: "outline",
-    outline: true,
-  });
-  await remove(TMP);
+async function main() {
+	// Remove previous artifact
+	removeSync(SOLID_DIST);
+	removeSync(OUTLINE_DIST);
+
+	// Clone the original SVG icons from the repo.
+	const [solidGit, outlineGit] = [SOLID_SRC, OUTLINE_SRC].map((repo) =>
+		degit(repo, { cache: false, verbose: true, force: true })
+	);
+
+	await solidGit.clone(SOLID_DIST);
+	await outlineGit.clone(OUTLINE_DIST);
+
+	// Generate the icons in the proper folder
+	await generateIcons({ path: SOLID_DIST, name: "solid", outline: false });
+	await generateIcons({ path: OUTLINE_DIST, name: "outline", outline: true });
+
+	// Remove temporary git clones folder
+	await remove(TMP);
 }
 
 async function generateIcons({ path, name, outline }) {
-  const icons = await readdir(path);
-  const exportedIcons: string[] = [];
-  const exportedIconsCjs = [
-    "'use strict;'",
-    "Object.defineProperty(exports, '__esModule', { value: true });",
-  ];
-  const exportedTypes: string[] = [];
+	const icons = await readdir(path);
+	const exportedIcons: string[] = [];
+	const exportedTypes: string[] = [];
 
-  for (const icon of icons) {
-    const iconName = camelCase(parse(icon).name);
-    const iconSVG = await readFile(join(path, icon), { encoding: "utf-8" });
-    const cleanedSVG = iconSVG
-      .split("\n")
-      .filter(Boolean)
-      .map((path) => path.replace(/fill="(#\w+)"/g, 'fill="transparent"'));
-    cleanedSVG.shift();
-    cleanedSVG.pop();
+	for (const icon of icons) {
+		const iconName = camelCase(parse(icon).name);
+		const iconSVG = await readFile(join(path, icon), { encoding: "utf-8" });
 
-    const code = cleanedSVG.join(" ").replace(/\s{2,}/g, "");
-    const iconPathsStr = dd`export const ${iconName} = { path: \`${code}\`, outline: ${outline} };`;
-    const iconTypeStr = dd`export declare const ${iconName}: { path: string; outline: boolean; };`;
-    const iconPathsStrCjs = dd`exports.${iconName} = { path: \`${code}\`, outline: ${outline} };`;
-    exportedIcons.push(iconPathsStr);
-    exportedTypes.push(iconTypeStr);
-    exportedIconsCjs.push(iconPathsStrCjs);
-  }
+		// Clean the SVG markup
+		const cleanedSVG = iconSVG
+			.split("\n")
+			.filter(Boolean)
+			.map((path) => path.replace(/fill="(#\w+)"/g, 'fill="transparent"'));
+		cleanedSVG.shift();
+		cleanedSVG.pop();
 
-  const exportedIconsStr = exportedIcons.join("\n");
-  const exportedTypesStr = exportedTypes.join("\n");
-  const exportedIconsStrCjs = exportedIconsCjs.join("\n");
-  await outputFile(
-    join(process.cwd(), name, "index.esm.js"),
-    exportedIconsStr,
-    {
-      encoding: "utf-8",
-    }
-  );
-  await outputFile(join(process.cwd(), name, "index.d.ts"), exportedTypesStr, {
-    encoding: "utf-8",
-  });
-  await outputFile(
-    join(process.cwd(), name, "index.common.js"),
-    exportedIconsStrCjs,
-    {
-      encoding: "utf-8",
-    }
-  );
-  await outputFile(
-    join(process.cwd(), name, "package.json"),
-    dd`
+		const code = cleanedSVG.join(" ").replace(/\s{2,}/g, "");
+		const iconPathsStr = dd`export const ${iconName} = { path: \`${code}\`, outline: ${outline} };`;
+		const iconTypeStr = dd`export declare const ${iconName}: { path: string; outline: boolean; };`;
+		exportedIcons.push(iconPathsStr);
+		exportedTypes.push(iconTypeStr);
+	}
+
+	const exportedIconsStr = exportedIcons.join("\n");
+	const exportedTypesStr = exportedTypes.join("\n");
+	await outputFile(join(process.cwd(), name, "index.js"), exportedIconsStr, {
+		encoding: "utf-8",
+	});
+	await outputFile(join(process.cwd(), name, "index.d.ts"), exportedTypesStr, {
+		encoding: "utf-8",
+	});
+	await outputFile(
+		join(process.cwd(), name, "package.json"),
+		dd`
       {
         "name": "@amoutonbrady/solid-heroicons/${name}",
-        "main": "./index.common.js",
-        "module": "./index.esm.js",
-        "types": "./index.d.ts",
+				"type": "module",
+        "module": "index.js",
+        "main": "index.js",
+        "types": "index.d.ts",
+				"exports": {
+					".": {
+					  "default": "./index.js"
+					} 
+				},
         "sideEffects": false
       }`
-  );
+	);
 }
-
-main().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
