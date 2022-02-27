3dcfb1165b6d79bad6998f62258533a1ad87d348
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2022-02-27 01:16:10 +0100

    :wrench: Add server export mapping

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,10 @@
-import withRollup from 'rollup-preset-solid'
+import withRollup from "rollup-preset-solid";
 
-export default withRollup ({ input: "src/icon.tsx" });
+export default withRollup([
+  { input: "src/icon.tsx", mappingName: "browser" },
+  {
+    input: "src/icon.tsx",
+    mappingName: "server",
+    solidOptions: { generate: "ssr", hydratable: false },
+  },
+]);
