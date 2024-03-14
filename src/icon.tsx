9f3d2529f3b812514da318624e6b3f2775c8e45e
Dovydas <dovydas@yoshke.org>
2024-03-14 14:46:35 +0000

    add Heroicons Micro

--- src/icon.tsx
+++ src/icon.tsx
@@ -8,6 +8,7 @@ interface Props extends JSX.SvgSVGAttributes<SVGSVGElement> {
     path: JSX.Element;
     outline?: boolean;
     mini?: boolean;
+    micro?: boolean;
   };
 }
 
@@ -17,17 +18,19 @@ interface Props extends JSX.SvgSVGAttributes<SVGSVGElement> {
  * a function returning all the path(s) to insert within the SVG element.
  *
  * It will take the parent CSS `color` value as fill/stroke depending on
- * whether it's imported from `outline`, `solid` or `solid-mini`.
+ * whether it's imported from `outline`, `solid`, `solid-mini`, or `solid-micro`.
  *
  * @example
  * ```tsx
  * import { arrowLeft } from 'solid-heroicons/outline'
  * import { arrowRight } from 'solid-heroicons/solid'
  * import { arrowDown } from 'solid-heroicons/solid-mini'
+ * import { arrowUp } from 'solid-heroicons/solid-micro'
  *
  * const icon = <Icon path={arrowLeft} class="text-gray-900 h-6" />
  * const icon2 = <Icon path={arrowRight} class="text-gray-900 h-6" />
  * const icon3 = <Icon path={arrowDown} class="text-gray-900 h-6" />
+ * const icon4 = <Icon path={arrowUp} class="text-gray-900 h-6" />
  * ```
  */
 export const Icon: Component<Props> = (props) => {
@@ -35,7 +38,7 @@ export const Icon: Component<Props> = (props) => {
 
   return (
     <svg
-      viewBox={internal.path.mini ? "0 0 20 20" : "0 0 24 24"}
+      viewBox={internal.path.micro ? "0 0 16 16" : (internal.path.mini ? "0 0 20 20" : "0 0 24 24")}
       fill={internal.path.outline ? "none" : "currentColor"}
       stroke={internal.path.outline ? "currentColor" : "none"}
       stroke-width={internal.path.outline ? 1.5 : undefined}
